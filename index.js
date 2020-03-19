186d76b750e9c525f3341efb2f95e2cbf0fbdd65
Daniel Sottile <danielsottile@gmail.com>
2020-03-19 12:06:19 -0400

    made delete function

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const STORE = [
-  {id: cuid(), name: "apples", checked: false},
-  {id: cuid(), name: "oranges", checked: false},
-  {id: cuid(), name: "milk", checked: true},
-  {id: cuid(), name: "bread", checked: false}
+  {id: cuid(), name: 'apples', checked: false},
+  {id: cuid(), name: 'oranges', checked: false},
+  {id: cuid(), name: 'milk', checked: true},
+  {id: cuid(), name: 'bread', checked: false}
 ];
 
 
 function generateItemElement(item) {
   return `
     <li data-item-id="${item.id}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -25,11 +25,11 @@ function generateItemElement(item) {
 
 
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
+  console.log('Generating shopping list element');
 
   const items = shoppingList.map((item) => generateItemElement(item));
   
-  return items.join("");
+  return items.join('');
 }
 
 
@@ -60,7 +60,7 @@ function handleNewItemSubmit() {
 }
 
 function toggleCheckedForListItem(itemId) {
-  console.log("Toggling checked property for item with id " + itemId);
+  console.log(`Toggling checked property for item with id ${itemId}`);
   const item = STORE.find(item => item.id === itemId);
   item.checked = !item.checked;
 }
@@ -73,7 +73,7 @@ function getItemIdFromElement(item) {
 }
 
 function handleItemCheckClicked() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     console.log('`handleItemCheckClicked` ran');
     const id = getItemIdFromElement(event.currentTarget);
     toggleCheckedForListItem(id);
@@ -81,11 +81,28 @@ function handleItemCheckClicked() {
   });
 }
 
+// they tried to trick us.  we need to add a function that deletes; it should be similar to the toggleCheckedForListItem function
+
+function deleteListItem(itemId) {
+  console.log(`deleting item with id ${itemId}`);
+  // similar to the toggle function, we're looking for something using the itemID;  in this case, we want the itemIndex so we can use .splice
+  const itemIndex = STORE.findIndex(i=> i.id === itemId);
+  // this is the only array method that can delete in place without creating a new array; we use the itemIndex before and add 1 so we can just delete the singluar item
+  STORE.splice(itemIndex, 1);
+}
+
 
 function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
-  // item
-  console.log('`handleDeleteItemClicked` ran')
+  // first thing we do is the same as the handleItemCheckClicked, we listen to the .js-shopping-list and any .js-item-delete buttons
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    console.log('`handleDeleteItemClicked` ran');
+    // likse above, we get the id from the currentTarget, which is our button
+    const id = getItemIdFromElement(event.currentTarget);
+    // we made a function that deletes it, so look above
+    deleteListItem(id);
+    // render the list
+    renderShoppingList();
+  });
 }
 
 // this function will be our callback when the page loads. it's responsible for
